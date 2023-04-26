28fa2e7b56fae305ddd2947b770ea19338e90487
Bleachigo <artwhite2012@gmail.com>
2023-04-26 16:04:53 +0300

    Update script.js

--- super-awesome-slider/script.js
+++ super-awesome-slider/script.js
@@ -1,15 +1,15 @@
 const track = document.querySelector('.img-track'); // get the slides wrapper
 
-window.onmousedown = e => {
+const handleOnDown = e => {
 	track.dataset.mouseDownAt = e.clientX; // get the "mousedown" at X axis and put it to data attr of wrapper
 }
 
-window.onmouseup = () => {
+const handleOnUp = () => {
 	track.dataset.mouseDownAt = "0";
 	track.dataset.prevPercentage = track.dataset.percentage;
 }
 
-window.onmousemove = e => {
+const handleOnMove = e => {
 	if (track.dataset.mouseDownAt === "0") return;
 
 	const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
@@ -31,3 +31,15 @@ window.onmousemove = e => {
 		}, { duration: 1200, fill: "forwards" });
 	}
 }
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
